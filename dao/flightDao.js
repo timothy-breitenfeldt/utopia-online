68f78164053462d686e2231cff38285791cf6276
timothy-breitenfeldt <timothy.breitenfeldt@smoothstack.com>
2020-02-24 01:10:15 -0500

    Created unit tests for /flights/search.

--- dao/flightDao.js
+++ dao/flightDao.js
@@ -23,6 +23,8 @@ function getFlights(searchParameters) {
             return "flight.origin IN (SELECT id FROM airport WHERE airport.city like ?)";
           } else if (k == "flight_number") {
             return "flight.id = ?";
+          } else if (k == "airport_id") {
+            return "airport.id=?";
           } else if (k.includes("date") || k == "airport_name") {
             return `${k} like ?`;
           } else if (k == "price") {
@@ -33,13 +35,13 @@ function getFlights(searchParameters) {
         })
         .join(" and ");
 
-      const sql = `SELECT flight.id as flight_number, airport.id as airport_id, airport.name airport_name, flight.capacity, flight.price, flight.departure_date, flight.arrival_date, dest, origin
+      const sql = `SELECT airport.id as airport_id, flight.id as flight_number, airport.name airport_name, flight.capacity, flight.price, flight.departure_date, flight.arrival_date, dest, origin
         FROM flight JOIN airport ON flight.origin = airport.id
         WHERE capacity > 0 AND ${conditions};
       `;
 
       db.connection.query(sql, parameters, function(error, result) {
-        return error ? reject(error) : resolve(result);
+        return error ? console.log(error) || reject(error) : resolve(result);
       });
     }
   });
--- test/onlineControllerTest.js
+++ test/onlineControllerTest.js
@@ -37,7 +37,7 @@ describe("onlineController", () => {
     });
   });
 
-  describe("/POST /flights/search flight_number=1", async () => {
+  describe("/POST /flights/search flight_number=1", () => {
     before(done => {
       const sql = `
         INSERT INTO airport (id, name, street, city, state, country, postal_code) VALUES(1, "PDX", "5th street", "Portland", "Oregon", "US", "98661");
@@ -47,7 +47,7 @@ describe("onlineController", () => {
       db.connection.query(sql, () => done());
     });
 
-    it("it should get a flight with an ID of 1", done => {
+    it("it should get a flight with an flight_id of 1", done => {
       chai
         .request(server)
         .post("/online/flights/search")
@@ -65,6 +65,317 @@ describe("onlineController", () => {
           result.body[0].should.have.property("arrival_date");
           result.body[0].should.have.property("capacity");
           result.body[0].should.have.property("price");
+          done();
+        });
+    });
+  });
+
+  describe("/POST /flights/search airport_id=1, (dest=2 and origin=1)", () => {
+    before(done => {
+      const sql = `
+        INSERT INTO airport (id, name, street, city, state, country, postal_code) VALUES(1, "PDX", "5th street", "Portland", "Oregon", "US", "98661");
+        INSERT INTO airport (id, name, street, city, state, country, postal_code) VALUES(2, "Seatac", "3rd Ave.", "Seattle", "Oregon", "US", "98661");
+        INSERT INTO flight (id, dest, origin, capacity, price, arrival_date, departure_date) VALUES(1, 2, 1, 20, 50.0, "2019-11-06", "2019-11-07");
+      `;
+      db.connection.query(sql, () => done());
+    });
+
+    it("it should get a flight with an airport_id of 1", done => {
+      chai
+        .request(server)
+        .post("/online/flights/search")
+        .send({ airport_id: 1 })
+        .end((error, result) => {
+          result.should.have.status(200);
+          result.body.should.be.a("array");
+          result.body.length.should.be.eql(1);
+          result.body[0].should.be.a("object");
+          result.body[0].should.have.property("flight_number");
+          result.body[0].should.have.property("airport_id");
+          result.body[0].should.have.property("airport_name");
+          result.body[0].should.have.property("dest");
+          result.body[0].should.have.property("origin");
+          result.body[0].should.have.property("departure_date");
+          result.body[0].should.have.property("arrival_date");
+          result.body[0].should.have.property("capacity");
+          result.body[0].should.have.property("price");
+          done();
+        });
+    });
+  });
+
+  describe("/POST /flights/search airport_id=1, (dest=1 and origin=2)", () => {
+    before(done => {
+      const sql = `
+        INSERT INTO airport (id, name, street, city, state, country, postal_code) VALUES(1, "PDX", "5th street", "Portland", "Oregon", "US", "98661");
+        INSERT INTO airport (id, name, street, city, state, country, postal_code) VALUES(2, "Seatac", "3rd Ave.", "Seattle", "Oregon", "US", "98661");
+        INSERT INTO flight (id, dest, origin, capacity, price, arrival_date, departure_date) VALUES(1, 1, 2, 20, 50.0, "2019-11-06", "2019-11-07");
+      `;
+      db.connection.query(sql, () => done());
+    });
+
+    it("it should get a flight with an airport_id of 1", done => {
+      chai
+        .request(server)
+        .post("/online/flights/search")
+        .send({ airport_id: 1 })
+        .end((error, result) => {
+          result.should.have.status(200);
+          result.body.should.be.a("array");
+          result.body.length.should.be.eql(0);
+          done();
+        });
+    });
+  });
+
+  describe("/POST /flights/search arrival_date=2019-11-07", () => {
+    before(done => {
+      const sql = `
+        INSERT INTO airport (id, name, street, city, state, country, postal_code) VALUES(1, "PDX", "5th street", "Portland", "Oregon", "US", "98661");
+        INSERT INTO airport (id, name, street, city, state, country, postal_code) VALUES(2, "Seatac", "3rd Ave.", "Seattle", "Oregon", "US", "98661");
+        INSERT INTO flight (id, dest, origin, capacity, price, arrival_date, departure_date) VALUES(1, 2, 1, 20, 50.0, "2019-11-07 00:00:00.00", "2019-11-06 00:00:00.00");
+      `;
+      db.connection.query(sql, () => done());
+    });
+
+    it("it should get a flight with an arrival-date  of 2019-11-07", done => {
+      chai
+        .request(server)
+        .post("/online/flights/search")
+        .send({ arrival_date: "2019-11-07" })
+        .end((error, result) => {
+          result.should.have.status(200);
+          result.body.should.be.a("array");
+          result.body.length.should.be.eql(1);
+          result.body[0].should.be.a("object");
+          result.body[0].should.have.property("flight_number");
+          result.body[0].should.have.property("airport_id");
+          result.body[0].should.have.property("airport_name");
+          result.body[0].should.have.property("dest");
+          result.body[0].should.have.property("origin");
+          result.body[0].should.have.property("departure_date");
+          result.body[0].should.have.property("arrival_date");
+          result.body[0].should.have.property("capacity");
+          result.body[0].should.have.property("price");
+          done();
+        });
+    });
+  });
+
+  describe("/POST /flights/search departure_date=2019-11-05", () => {
+    before(done => {
+      const sql = `
+        INSERT INTO airport (id, name, street, city, state, country, postal_code) VALUES(1, "PDX", "5th street", "Portland", "Oregon", "US", "98661");
+        INSERT INTO airport (id, name, street, city, state, country, postal_code) VALUES(2, "Seatac", "3rd Ave.", "Seattle", "Oregon", "US", "98661");
+        INSERT INTO flight (id, dest, origin, capacity, price, arrival_date, departure_date) VALUES(1, 2, 1, 20, 50.0, "2019-11-07 00:00:00.00", "2019-11-05");
+      `;
+      db.connection.query(sql, () => done());
+    });
+
+    it("it should get a flight with an departure_date of 2019-11-05", done => {
+      chai
+        .request(server)
+        .post("/online/flights/search")
+        .send({ departure_date: "2019-11-05" })
+        .end((error, result) => {
+          result.should.have.status(200);
+          result.body.should.be.a("array");
+          result.body.length.should.be.eql(1);
+          result.body[0].should.be.a("object");
+          result.body[0].should.have.property("flight_number");
+          result.body[0].should.have.property("airport_id");
+          result.body[0].should.have.property("airport_name");
+          result.body[0].should.have.property("dest");
+          result.body[0].should.have.property("origin");
+          result.body[0].should.have.property("departure_date");
+          result.body[0].should.have.property("arrival_date");
+          result.body[0].should.have.property("capacity");
+          result.body[0].should.have.property("price");
+          done();
+        });
+    });
+  });
+
+  describe("/POST /flights/search dest=seattle", () => {
+    before(done => {
+      const sql = `
+        INSERT INTO airport (id, name, street, city, state, country, postal_code) VALUES(1, "PDX", "5th street", "Portland", "Oregon", "US", "98661");
+        INSERT INTO airport (id, name, street, city, state, country, postal_code) VALUES(2, "Seatac", "3rd Ave.", "Seattle", "Oregon", "US", "98661");
+        INSERT INTO flight (id, dest, origin, capacity, price, arrival_date, departure_date) VALUES(1, 2, 1, 20, 50.0, "2019-11-07 00:00:00.00", "2019-11-05");
+      `;
+      db.connection.query(sql, () => done());
+    });
+
+    it("it should get a flight with an dest of seattle", done => {
+      chai
+        .request(server)
+        .post("/online/flights/search")
+        .send({ dest: "seattle" })
+        .end((error, result) => {
+          result.should.have.status(200);
+          result.body.should.be.a("array");
+          result.body.length.should.be.eql(1);
+          result.body[0].should.be.a("object");
+          result.body[0].should.have.property("flight_number");
+          result.body[0].should.have.property("airport_id");
+          result.body[0].should.have.property("airport_name");
+          result.body[0].should.have.property("dest");
+          result.body[0].should.have.property("origin");
+          result.body[0].should.have.property("departure_date");
+          result.body[0].should.have.property("arrival_date");
+          result.body[0].should.have.property("capacity");
+          result.body[0].should.have.property("price");
+          done();
+        });
+    });
+  });
+
+  describe("/POST /flights/search origin=Portland", () => {
+    before(done => {
+      const sql = `
+        INSERT INTO airport (id, name, street, city, state, country, postal_code) VALUES(1, "PDX", "5th street", "Portland", "Oregon", "US", "98661");
+        INSERT INTO airport (id, name, street, city, state, country, postal_code) VALUES(2, "Seatac", "3rd Ave.", "Seattle", "Oregon", "US", "98661");
+        INSERT INTO flight (id, dest, origin, capacity, price, arrival_date, departure_date) VALUES(1, 2, 1, 20, 50.0, "2019-11-07 00:00:00.00", "2019-11-05");
+      `;
+      db.connection.query(sql, () => done());
+    });
+
+    it("it should get a flight with an origin  of Portland", done => {
+      chai
+        .request(server)
+        .post("/online/flights/search")
+        .send({ origin: "Portland" })
+        .end((error, result) => {
+          result.should.have.status(200);
+          result.body.should.be.a("array");
+          result.body.length.should.be.eql(1);
+          result.body[0].should.be.a("object");
+          result.body[0].should.have.property("flight_number");
+          result.body[0].should.have.property("airport_id");
+          result.body[0].should.have.property("airport_name");
+          result.body[0].should.have.property("dest");
+          result.body[0].should.have.property("origin");
+          result.body[0].should.have.property("departure_date");
+          result.body[0].should.have.property("arrival_date");
+          result.body[0].should.have.property("capacity");
+          result.body[0].should.have.property("price");
+          done();
+        });
+    });
+  });
+
+  describe("/POST /flights/search price=100", () => {
+    before(done => {
+      const sql = `
+        INSERT INTO airport (id, name, street, city, state, country, postal_code) VALUES(1, "PDX", "5th street", "Portland", "Oregon", "US", "98661");
+        INSERT INTO airport (id, name, street, city, state, country, postal_code) VALUES(2, "Seatac", "3rd Ave.", "Seattle", "Oregon", "US", "98661");
+        INSERT INTO flight (id, dest, origin, capacity, price, arrival_date, departure_date) VALUES(1, 2, 1, 20, 50.0, "2019-11-07 00:00:00.00", "2019-11-05");
+      `;
+      db.connection.query(sql, () => done());
+    });
+
+    it("it should get a flight with a price of 100", done => {
+      chai
+        .request(server)
+        .post("/online/flights/search")
+        .send({ price: "100" })
+        .end((error, result) => {
+          result.should.have.status(200);
+          result.body.should.be.a("array");
+          result.body.length.should.be.eql(1);
+          result.body[0].should.be.a("object");
+          result.body[0].should.have.property("flight_number");
+          result.body[0].should.have.property("airport_id");
+          result.body[0].should.have.property("airport_name");
+          result.body[0].should.have.property("dest");
+          result.body[0].should.have.property("origin");
+          result.body[0].should.have.property("departure_date");
+          result.body[0].should.have.property("arrival_date");
+          result.body[0].should.have.property("capacity");
+          result.body[0].should.have.property("price");
+          done();
+        });
+    });
+  });
+
+  describe("/POST /flights/search price=50", () => {
+    before(done => {
+      const sql = `
+        INSERT INTO airport (id, name, street, city, state, country, postal_code) VALUES(1, "PDX", "5th street", "Portland", "Oregon", "US", "98661");
+        INSERT INTO airport (id, name, street, city, state, country, postal_code) VALUES(2, "Seatac", "3rd Ave.", "Seattle", "Oregon", "US", "98661");
+        INSERT INTO flight (id, dest, origin, capacity, price, arrival_date, departure_date) VALUES(1, 2, 1, 20, 50.0, "2019-11-07 00:00:00.00", "2019-11-05");
+      `;
+      db.connection.query(sql, () => done());
+    });
+
+    it("it should get a flight with a price of 50", done => {
+      chai
+        .request(server)
+        .post("/online/flights/search")
+        .send({ price: "50" })
+        .end((error, result) => {
+          result.should.have.status(200);
+          result.body.should.be.a("array");
+          result.body.length.should.be.eql(1);
+          result.body[0].should.be.a("object");
+          result.body[0].should.have.property("flight_number");
+          result.body[0].should.have.property("airport_id");
+          result.body[0].should.have.property("airport_name");
+          result.body[0].should.have.property("dest");
+          result.body[0].should.have.property("origin");
+          result.body[0].should.have.property("departure_date");
+          result.body[0].should.have.property("arrival_date");
+          result.body[0].should.have.property("capacity");
+          result.body[0].should.have.property("price");
+          done();
+        });
+    });
+  });
+
+  describe("/POST /flights/search price=49", () => {
+    before(done => {
+      const sql = `
+        INSERT INTO airport (id, name, street, city, state, country, postal_code) VALUES(1, "PDX", "5th street", "Portland", "Oregon", "US", "98661");
+        INSERT INTO airport (id, name, street, city, state, country, postal_code) VALUES(2, "Seatac", "3rd Ave.", "Seattle", "Oregon", "US", "98661");
+        INSERT INTO flight (id, dest, origin, capacity, price, arrival_date, departure_date) VALUES(1, 2, 1, 20, 50.0, "2019-11-07 00:00:00.00", "2019-11-05");
+      `;
+      db.connection.query(sql, () => done());
+    });
+
+    it("it should get a flight with a price of 49", done => {
+      chai
+        .request(server)
+        .post("/online/flights/search")
+        .send({ price: "49" })
+        .end((error, result) => {
+          result.should.have.status(200);
+          result.body.should.be.a("array");
+          result.body.length.should.be.eql(0);
+          done();
+        });
+    });
+  });
+
+  describe("/POST /flights/search abcde=20", () => {
+    before(done => {
+      const sql = `
+        INSERT INTO airport (id, name, street, city, state, country, postal_code) VALUES(1, "PDX", "5th street", "Portland", "Oregon", "US", "98661");
+        INSERT INTO airport (id, name, street, city, state, country, postal_code) VALUES(2, "Seatac", "3rd Ave.", "Seattle", "Oregon", "US", "98661");
+        INSERT INTO flight (id, dest, origin, capacity, price, arrival_date, departure_date) VALUES(1, 2, 1, 20, 50.0, "2019-11-07 00:00:00.00", "2019-11-05");
+      `;
+      db.connection.query(sql, () => done());
+    });
+
+    it("it should throw a 400  error with a param of abcde equal to 20", done => {
+      chai
+        .request(server)
+        .post("/online/flights/search")
+        .send({ abcde: "20" })
+        .end((error, result) => {
+          result.should.have.status(400);
+          result.body.should.be.a("object");
+          result.body.should.have.property("status");
+          result.body.should.have.property("message");
           done();
         });
     });
